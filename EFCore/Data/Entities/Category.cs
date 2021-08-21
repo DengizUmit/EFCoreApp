b3ba5de91f6bf46cf2eb56261438517c8ad4018b
Ümit Dengiz <umit.dengiz@enqura.com>
2021-08-22 00:42:21 +0300

    Data Annotations Table

--- EFCore/Data/Entities/Category.cs
+++ EFCore/Data/Entities/Category.cs
@@ -1,10 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace EFCore.Data.Entities
 {
+    [Table(name: "Category", Schema = "c")]
     public class Category
     {
         public int Id { get; set; }
---
+++ EFCore/Migrations/20210821213727_AddTableName.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace EFCore.Migrations
+{
+    public partial class AddTableName : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Categories",
+                table: "Categories");
+
+            migrationBuilder.EnsureSchema(
+                name: "c");
+
+            migrationBuilder.RenameTable(
+                name: "Categories",
+                newName: "Category",
+                newSchema: "c");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Category",
+                schema: "c",
+                table: "Category",
+                column: "Id");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Category",
+                schema: "c",
+                table: "Category");
+
+            migrationBuilder.RenameTable(
+                name: "Category",
+                schema: "c",
+                newName: "Categories");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Categories",
+                table: "Categories",
+                column: "Id");
+        }
+    }
+}
