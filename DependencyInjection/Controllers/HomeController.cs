8a3eefa41f897ef2550443c1e753b100ffcf31de
Ümit Dengiz <umit.dengiz@enqura.com>
2021-08-29 15:01:05 +0300

    Dependency Injection

---
+++ DependencyInjection/Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DependencyInjection.Controllers
+{
+    public class HomeController : Controller
+    {
+        private IProductService _productService;
+        public HomeController(IProductService productService)
+        {
+            _productService = productService;
+        }
+
+        public IActionResult Index()
+        {
+            var total = _productService.GetTotal();
+            return View();
+        }
+    }
+
+    public interface IProductService
+    {
+        int GetTotal();
+    }
+
+    public class ProductManager : IProductService
+    {
+        public int GetTotal()
+        {
+            return 40;
+        }
+    }
+}
